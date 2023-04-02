8869e65667fd10f7b0e608c38f8da71e45027570
Ricky Rodrigo <rickyrodrigo.ph@gmail.com>
2023-04-02 22:57:26 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
+DFRobotMaqueenPlus.I2CInit()
+huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
 basic.forever(function () {
-    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 100)
+    huskylens.request()
+    if (huskylens.isLearned(1)) {
+        if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+                DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 100)
+            }
+        }
+    }
 })
